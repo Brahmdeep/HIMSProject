a387affb4053ad2f223e34ab86635bc20780ff45
Brahmdeep Singh <brahmdeeps59@gmail.com>
2018-06-14 12:15:16 +0530

    PRoject final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { SigInComponent } from './sig-in/sig-in.component';
+import { MedicalStafRegComponent } from './medical-staf-reg/medical-staf-reg.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'signin',component:SigInComponent},
+  {path:'departments',component:DepartmentListComponent},
+  {path:'employee',component:EmployeeListComponent},
+  {path:'medpathreg',component:MedicalStafRegComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent=[MedicalStafRegComponent,DepartmentListComponent,EmployeeListComponent,SigInComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigInComponent } from './sig-in/sig-in.component';
+import { MedicalStafRegComponent } from './medical-staf-reg/medical-staf-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponent,
+    SigInComponent,
+    MedicalStafRegComponent
   ],
   imports: [
     BrowserModule,
